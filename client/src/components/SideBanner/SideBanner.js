675184aac2aa61526f558255406b9c234ff2129b
Kevin Alvarez <kevin.alvarez@trueforce.com>
2021-04-17 16:37:00 -0400

    feat: abstract side banner

---
+++ client/src/components/SideBanner/SideBanner.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { Grid, Typography } from "@material-ui/core";
+
+import { ReactComponent as BubbleIcon } from "../../assets/bubble.svg";
+
+import "./SideBanner.css";
+
+const SideBanner = (props) => {
+  const { children } = props;
+  return (
+    <Grid
+      container
+      justify="flex-start"
+      alignItems="stretch"
+    >
+      <Grid item lg={5} md={5} justify="center">
+        <div class="side-banner side-banner__background">
+          <div class="side-banner__text-box">
+            <Grid container justify="center">
+              <div className="side-banner__bubble">
+                <BubbleIcon />
+              </div>
+              <Typography variant="h4" align="center" color="secondary">
+                Converse with anyone with any language
+              </Typography>
+            </Grid>
+          </div>
+        </div>
+      </Grid>
+      <Grid item md={7} lg={7}>
+        {children}
+      </Grid>
+    </Grid>
+  )
+};
+
+export default SideBanner;
--- client/src/pages/Signup/Signup.js
+++ client/src/pages/Signup/Signup.js
@@ -5,15 +5,12 @@ import {
   Grid,
   Typography,
   Box,
-  Button,
-  Container
+  Button
 } from "@material-ui/core";
 
 import SignupForm from "./SignupForm";
+import SideBanner from "../../components/SideBanner";
 import { register } from "../../store/utils/thunkCreators";
-import { ReactComponent as BubbleIcon } from '../../assets/bubble.svg';
-
-import './Signup.css';
 
 const Signup = (props) => {
   const { user, register } = props;
@@ -24,52 +21,32 @@ const Signup = (props) => {
   }
 
   return (
-    <Grid
-      container
-      justify="flex-start"
-      alignItems="stretch"
-    >
-      <Grid item lg={5} md={5} justify="center">
-        <div class="side-banner side-banner__background">
-          <div class="side-banner__text-box">
-            <Grid container justify="center">
-              <div className="side-banner__bubble">
-                <BubbleIcon />
-              </div>
-              <Typography variant="h4" align="center" color="secondary">
-                Converse with anyone with any language
-              </Typography>
-            </Grid>
-          </div>
-        </div>
-      </Grid>
-      <Grid item md={7} lg={7}>
-        <Grid container direction="column" justify="flex-start" alignItems="stretch">
-          <Grid container item justify="flex-end" alignItems="center">
-            <Box m={2}>
-              <Typography>Need to log in?</Typography>
-            </Box>
-            <Box m={2}>
-              <Button
-                size="large"
-                color="secondary"
-                variant="contained"
-                onClick={() => history.push("/login")}
-              >
-                Login
-                </Button>
-            </Box>
-          </Grid>
-          <Box height="100%" display="flex" flex="1">
-            <Grid container item justify="center" alignItems="center">
-              <Box width="60%">
-                <SignupForm register={register} />
-              </Box>
-            </Grid>
+    <SideBanner>
+      <Grid container direction="column" justify="flex-start" alignItems="stretch">
+        <Grid container item justify="flex-end" alignItems="center">
+          <Box m={2}>
+            <Typography>Need to log in?</Typography>
+          </Box>
+          <Box m={2}>
+            <Button
+              size="large"
+              color="secondary"
+              variant="contained"
+              onClick={() => history.push("/login")}
+            >
+              Login
+            </Button>
           </Box>
         </Grid>
+        <Box height="100%" display="flex" flex="1">
+          <Grid container item justify="center" alignItems="center">
+            <Box width="60%">
+              <SignupForm register={register} />
+            </Box>
+          </Grid>
+        </Box>
       </Grid>
-    </Grid>
+    </SideBanner>
   );
 };
 
