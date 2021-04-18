14c57cc695f6b78cf334d8ad46b6a372e4d70816
Kevin Alvarez <kevin.alvarez@trueforce.com>
2021-04-18 18:53:55 -0400

    enh: auth socket connection

--- client/src/services/socket.js
+++ client/src/services/socket.js
@@ -6,21 +6,42 @@ import {
   addOnlineUser,
 } from "../store/conversations";
 
-const socket = io(window.location.origin);
+const socket = io(window.location.origin, { autoConnect: false });
 
-socket.on("connect", () => {
-  console.log("connected to server");
+export const getSocket = () => socket;
 
-  socket.on("add-online-user", (id) => {
-    store.dispatch(addOnlineUser(id));
+export const connect = (token) => {
+  const socket = getSocket();
+  socket.auth = { token };
+  socket.connect();
+
+  setListeners(socket);
+};
+
+export const disconnect = () => {
+  const socket = getSocket();
+  socket.auth = null;
+  socket.disconnect();
+};
+
+export const emit = (event, payload) => {
+  const socket = getSocket();
+  socket.emit(event, payload);
+};
+
+const setListeners = (socket) => {
+  socket.on("connect", () => {
+    console.log("connected to server");
+
+    socket.on("add-online-user", (id) => {
+      store.dispatch(addOnlineUser(id));
+    });
+
+    socket.on("remove-offline-user", (id) => {
+      store.dispatch(removeOfflineUser(id));
+    });
+    socket.on("new-message", (data) => {
+      store.dispatch(setNewMessage(data.message, data.sender));
+    });
   });
-
-  socket.on("remove-offline-user", (id) => {
-    store.dispatch(removeOfflineUser(id));
-  });
-  socket.on("new-message", (data) => {
-    store.dispatch(setNewMessage(data.message, data.sender));
-  });
-});
-
-export default socket;
+};
--- server/services/cache.js
+++ server/services/cache.js
@@ -2,14 +2,14 @@ const redis = require('redis');
 const { promisify } = require("util");
 
 let client;
-let getAsync;
+let hgetallAsync;
 let hsetAsync;
 let expireAsync;
 
 const connect = () => {
   try {
     client = redis.createClient(process.env.REDIS_URL);
-    getAsync = promisify(client.get).bind(client);
+    hgetallAsync = promisify(client.hgetall).bind(client);
     hsetAsync = promisify(client.hset).bind(client);
     expireAsync = promisify(client.expire).bind(client);
   } catch (error) {
@@ -18,7 +18,7 @@ const connect = () => {
   }
 };
 
-const get = async (key) => getAsync(key);
+const get = async (key) => hgetallAsync(key);
 
 const set = async (key, ...rest) => hsetAsync(key, ...rest);
 
