8f1ac2e0cb17bc90d87de47a8bda2c35e13e8abf
Kevin Alvarez <kevin.alvarez@trueforce.com>
2021-04-18 16:49:59 -0400

    enh: improve error handling

--- server/middlewares/auth-sockets.js
+++ server/middlewares/auth-sockets.js
@@ -3,31 +3,37 @@ const { User } = require("../db/models");
 
 const auth = () => {
   return async (socket, next) => {
-    const token = socket.handshake.auth.token;
+    try {
+      const token = socket.handshake.auth.token;
+      console.info(token);
 
-    if (!token) {
-      console.debug('Token not provided');
-      return next(new Error("Unathorized connection"));
+      if (!token) {
+        console.debug('Token not provided');
+        return next(new Error("Unathorized connection"));
+      }
+
+      const decoded = verifyToken(token);
+
+      if (!decoded) {
+        console.debug('Invalid token');
+        return next(new Error("Unathorized connection"));
+      }
+
+      const user = await User.findOne({
+        where: { id: decoded.id },
+      });
+
+      if (!user) {
+        console.debug('User not found');
+        return next(new Error("Unathorized connection"));
+      }
+
+      socket.userId = user.id;
+      next();
+    } catch (error) {
+      console.error(error);
+      next(new Error("Unathorized connection"));
     }
-
-    const decoded = verifyToken(token);
-
-    if (!decoded) {
-      console.debug('Invalid token');
-      return next(new Error("Unathorized connection"));
-    }
-
-    const user = await User.findOne({
-      where: { id: decoded.id },
-    });
-
-    if (!user) {
-      console.debug('User not found');
-      return next(new Error("Unathorized connection"));
-    }
-
-    socket.userId = user.id;
-    next();
   };
 };
 
